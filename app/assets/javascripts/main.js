81e56e0277b60808d17a461f608ce0b4b172fe9f
Johnny Hu <jgeehen.hu@gmail.com>
2016-09-26 12:31:34 -0700

    Add popup message

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
+// Checks for existing session, creating one if none exist.
+document.addEventListener("turbolinks:load", function() {
+  if (sessionStorage.getItem("brite-markets") !== "true") {
+    $(".popup").css( { "display":"flex" } );
+    sessionStorage.setItem("brite-markets", "true");
+  };
+});
+
+// Closes the pop up message.
+document.addEventListener("turbolinks:load", function() {
+  $(".popup").on("click", function() {
+    $(".popup").css( {"display":"none"} );
+  });
+});
+
+// Closes the flash message.
+document.addEventListener("turbolinks:load", function() {
   $(".flash__close").on( "click", function() {
     $(".flash--success, .flash--alert").css( { "display":"none" } );
   });
